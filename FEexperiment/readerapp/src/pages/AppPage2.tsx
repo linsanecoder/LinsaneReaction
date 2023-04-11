b2403cd79cd7000df388e6d6e2da34e08d5ee76a
linsanecoder <rapunzel82@hotmail.com>
2023-04-10 17:08:25 -0700

    Updated AppPage2 to fake api call to show map

--- FEexperiment/readerapp/src/pages/AppPage2.tsx
+++ FEexperiment/readerapp/src/pages/AppPage2.tsx
@@ -1,15 +1,17 @@
 import '../App.css';
 import Collapse from '../components/Collapse';
+import { getCategories } from "../apis/fakeCategoriesApi"
 
 function AppPage2() {
+  const categories = getCategories();
+
   return (
     <div className="App2">
-      <Collapse id="pg2obj1" label="Page 2 Object 1 React Content">
-        <p>Object 1 section</p>
+      {categories.map(cat => (
+      <Collapse id={cat.id} label={cat.name}>
+        <p>Object {cat.id} section</p>
       </Collapse>
-      <Collapse id="pg2obj2" label="Page 2 Object 2 React Content">
-        <p>Object 2 section</p>
-      </Collapse>
+      ))}
     </div>
   );
 }
